c8b8360c1df13ce2a439ee97d331f05070eb5b42
pkruczek <piotrek628@gmail.com>
2022-02-08 22:41:09 +0000

    add monitor

--- lib/machine/server.ex
+++ lib/machine/server.ex
@@ -5,7 +5,7 @@ defmodule Machine.Server do
   def init(address) do
     # TODO: maybe an Application.get_env
     :timer.send_interval(:timer.seconds(5), self(), :tick)
-    {:ok, %{address: address, task: nil, enabled: false}}
+    {:ok, %{address: address, task: nil, enabled: false, listeners: %{}}}
   end
 
   def start_link(address) do
@@ -20,9 +20,28 @@ defmodule Machine.Server do
     GenServer.call(pid, :enabled)
   end
 
+  def subscribe(pid) do
+    GenServer.call(pid, :subscribe)
+  end
+
   @impl true
   def handle_call(:enabled, _, state) do
     {:reply, state.enabled, state}
+  end
+
+  @impl true
+  def handle_call(:subscribe, {pid, _}, %{listeners: listeners} = state) do
+    if Map.has_key?(listeners, pid) do
+        {:reply, :already_listener, state}
+    else
+        ref = Process.monitor(pid)
+        {:reply, :ok, %{state | listeners: Map.put(listeners, pid, ref)}}
+    end
+  end
+
+  @impl true
+  def handle_info(:tick, %{listeners: listeners} = state) when map_size(listeners) == 0 do
+      {:stop, :normal, state}
   end
 
   @impl true
@@ -49,6 +68,11 @@ defmodule Machine.Server do
   def handle_info({:DOWN, ref, :process, _pid, _reason}, %{task: %{ref: ref}} = state) do
     # TODO: enabled: false?
     {:noreply, %{state | enabled: false, task: nil}}
+  end
+
+  @impl true
+  def handle_info({:DOWN, _ref, :process, pid, _reason}, %{listeners: listeners} = state) do
+     {:noreply, %{state | listeners: Map.delete(listeners, pid)}} 
   end
 
   defp via_tuple(machine_address) do
